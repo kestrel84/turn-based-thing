03eb757bf6196c8fa4f0c9609ba917debd7582d8
kestrel84 <maximilian.ashton@gmail.com>
2026-03-22 17:31:15 +0000

    initial commit

---
+++ cursor.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+@onready var map: TileMapLayer = $"../map"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mousepos = get_global_mouse_position()
+	position = map.map_to_local(map.local_to_map(Vector2(mousepos.x, mousepos.y - 24)))
+	position.y += 24
+	pass
