81e5ddcedd5092d670c7ce0158f39b5aafa5c447
kestrel84 <maximilian.ashton@gmail.com>
2026-05-03 19:15:34 +0100

    added enemies which can get hit

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 @onready var map: TileMapLayer = $"../map"
-@onready var attack_hitbox: Sprite2D = $"attack"
+@onready var attack_hitbox: Area2D = $"attack"
 @onready var cursor: Sprite2D = $"cursor"
 
 var attacking: bool = false;
@@ -22,20 +22,21 @@ func _process(delta: float) -> void:
 			position = map.map_to_local(map.local_to_map(Vector2(mousepos.x, mousepos.y)))
 	
 	if Input.is_action_just_pressed("attack one"):
-		trigger_attack()
-		
+		if attacking:
+			cancel_attack()
+		else:
+			trigger_attack()
+	
+	if Input.is_action_just_pressed("right click"):
+		if attacking:
+			cancel_attack()
+	
 	if attacking:
 		var local_mouse_pos = to_local(mousepos)
 		var attack_pos = local_mouse_pos.normalized() * 222
 		var map_attack_pos = map.map_to_local(map.local_to_map(to_global(attack_pos)))
 		attack_hitbox.global_position = map_attack_pos
-		attack_hitbox.rotation = map_attack_pos.angle()
-		print(snappedf(map_attack_pos.angle(), 0.001))
-		
-		
-		
-		
-		
+		attack_hitbox.rotation = to_local(map_attack_pos).angle()
 
 func trigger_attack():
 	print("triggering attack")
@@ -43,8 +44,19 @@ func trigger_attack():
 	cursor.visible = false
 	attack_hitbox.visible = true
 
+func cancel_attack():
+	print("cancel attack")
+	attacking = false
+	cursor.visible = true
+	attack_hitbox.visible = false
+
 func attack():
 	print("attacking")
+	print("enemies hit: ")
+	var enemies = attack_hitbox.get_overlapping_areas()
+	if len(enemies) > 0 && enemies[0].get_script():
+		enemies[0].on_hit()
+	print(enemies)
 	attacking = false
 	cursor.visible = true
 	attack_hitbox.visible = false
